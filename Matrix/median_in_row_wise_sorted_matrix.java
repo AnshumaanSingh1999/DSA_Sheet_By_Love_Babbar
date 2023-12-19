144dd4f273326c0effe93fa464282a421375ab59
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-12-19 19:33:18 +0530

    Create median_in_row_wise_sorted_matrix.java

---
+++ Matrix/median_in_row_wise_sorted_matrix.java
@@ -0,0 +1,37 @@
+/*
+Problem Solved Successfully 
+*/
+package Matrix;
+import java.util.Arrays;
+import java.util.Scanner;
+public class median_in_row_wise_sorted_matrix {
+    static int median(int matrix[][], int R, int C) {
+        int a[]=new int[R*C];
+        int k=0;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                a[k]=matrix[i][j];
+                k++;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        Arrays.sort(a);
+        int mi=a.length/2;
+        int c=a[mi];
+        return c;    
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int R=sc.nextInt();
+        int C=sc.nextInt();
+        int M[][]=new int[R][C];
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                M[i][j]=sc.nextInt();
+            }
+        }
+        int resp=median(M, R, C);
+    }
+}
